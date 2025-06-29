c9ab8c73ef97bc91cf6518a1f4f269c7f91a5fd7
Philipicalt <philipigariglio@gmail.com>
2025-06-29 16:08:09 -0300

    fix solicitacao ajuda

--- AcessibilidadeWebAPI/Dtos/SolicitacaoAjuda/SolicitacaoAjudaDto.cs
+++ AcessibilidadeWebAPI/Dtos/SolicitacaoAjuda/SolicitacaoAjudaDto.cs
@@ -1,12 +1,17 @@
-﻿namespace AcessibilidadeWebAPI.Dtos.SolicitacaoAjuda
+﻿using AcessibilidadeWebAPI.Models.Auth;
+
+namespace AcessibilidadeWebAPI.Dtos.SolicitacaoAjuda
 {
     public class SolicitacaoAjudaDto
     {
         public int IdSolicitacaoAjuda { get; set; }
-        public int IdUsuario { get; set; }
+        public int DeficienteUsuarioId { get; set; }
         public string Descricao { get; set; }
-        public int Status { get; set; }
-        public DateTimeOffset DataSolicitacao { get; set; }
-        public DateTimeOffset DataResposta { get; set; }
+        public StatusSolicitacao Status { get; set; }
+        public DateTime DataSolicitacao { get; set; }
+        public DateTime? DataResposta { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public string? EnderecoReferencia { get; set; }
     }
 }
